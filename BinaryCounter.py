2df7634e4ee917683afa45eda4fee66e1e670f04
Emma-sels <65134320+Emma-sels@users.noreply.github.com>
2021-01-02 13:43:29 -0600

    Add files via upload

---
+++ BinaryCounter.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO #importing the RPi GPIO library
+from time import sleep #function to add in delay later in code
+
+GPIO.setmode(GPIO.BOARD) #physical pin numbers
+GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW) #pin 7 (GPIO4) as an output that starts off
+GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW) #pin 11 (GPIO17) as an output that starts off
+GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW) #pin 13 (GPIO27) as an output that starts off
+GPIO.setup(15, GPIO.OUT, initial=GPIO.LOW) #pin 15 (GPIO22) as an output that starts off
+GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW) #pin 16 (GPIO23) as an output that starts off
+
+newnum = ' ';
+for i in range(31):
+    newnum = "{0:05b}".format(i)
+    print(newnum);
+    digitOne = newnum[0];
+    digitTwo = newnum[1];
+    digitThree = newnum[2];
+    digitFour = newnum[3];
+    digitFive = newnum[4];
+    if digitFive == '1' :
+        GPIO.output(7, GPIO.HIGH) # LED on
+    else :
+        GPIO.output(7, GPIO.LOW) # LED off
+    sleep(.5) # stay on .5 seconds
+    
+    if digitFour == '1' :
+        GPIO.output(11, GPIO.HIGH) # LED on
+    else :
+        GPIO.output(11, GPIO.LOW) # LED off
+    sleep(.5) # stay on .5 seconds
+    
+    if digitThree == '1' :
+        GPIO.output(13, GPIO.HIGH) # LED on
+    else :
+        GPIO.output(13, GPIO.LOW) # LED off
+    sleep(.5) # stay on .5 seconds
+    
+    if digitTwo == '1' :
+        GPIO.output(15, GPIO.HIGH) # LED on
+    else :
+        GPIO.output(15, GPIO.LOW) # LED off
+    sleep(.5) # stay on .5 seconds
+    
+    if digitOne == '1' :
+        GPIO.output(16, GPIO.HIGH) # LED on
+    else :
+        GPIO.output(16, GPIO.LOW) # LED off
+    sleep(.5) # stay on .5 seconds
+
